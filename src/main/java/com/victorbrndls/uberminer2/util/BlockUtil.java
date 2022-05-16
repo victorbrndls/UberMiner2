ad6f46d51e0cc59728ae2a899366d77055db55b1
Victor B <haristolhogamer@gmail.com>
2022-05-16 19:06:09 -0300

    Fix 1.18.2 dependencies

--- src/main/java/com/victorbrndls/uberminer2/util/BlockUtil.java
+++ src/main/java/com/victorbrndls/uberminer2/util/BlockUtil.java
@@ -8,15 +8,14 @@ public class BlockUtil {
     private static final ResourceLocation oreResourceLocation = new ResourceLocation("forge", "ores");
 
     public static boolean isOre(BlockState blockState) {
-        return blockState.getBlock().getTags().stream().anyMatch((tag) -> tag.equals(oreResourceLocation));
+        return blockState.getTags().anyMatch((tag) -> tag.location().equals(oreResourceLocation));
     }
 
     public static boolean isResource(BlockState blockState) {
         if (blockState.isAir()) return false;
         if (blockState.hasBlockEntity()) return false;
         if (blockState.getBlock().defaultDestroyTime() == -1f) return false;
-
-        return blockState.getBlock().getTags().stream().noneMatch((tag) -> tag.equals(oreResourceLocation));
+        return !isOre(blockState);
     }
 
 }
